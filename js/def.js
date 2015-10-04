f8d115f6683a6730c2305fda05fae6cf7d3b02ab
QC! Team <lecafe@qcteam.info>
2015-10-03 21:00:06 -0300

    Add a filter and minor fixes

--- js/def.js
+++ js/def.js
@@ -1,1 +1,38 @@
-/* JS File By TmzOS - QC! Team */ function calc(def, dmg){if(!dmg.isNaN && !def.isNaN){var v = Math.ceil(def*0.5);var v2 = Math.ceil(def*0.75);var d = Math.ceil(dmg-v);var d2 = Math.ceil(dmg-v2);if(d<1) d=1;if(d2<1) d2=1;document.getElementById("resultN").innerHTML = v;document.getElementById("resultE").innerHTML = v2;document.getElementById("resultND").innerHTML = d;document.getElementById("resultED").innerHTML = d2;}}
+/* JS File By TmzOS - QC! Team */ 
+
+function run()
+{
+	calc(document.getElementById('inputDefense').value, document.getElementById('inputDamage').value);
+}
+
+function calc(def, dmg)
+{
+	if(!dmg.isNaN && !def.isNaN)
+	{
+		var defs = def;
+		var dmgs = dmg;
+		
+		if(dmg < 0 || dmg > 999)
+		{
+			dmgs = 1;
+			document.getElementById('inputDamage').value = 1;
+		}
+		
+		if(def < 0 || def > 999)
+		{
+			defs = 1;
+			document.getElementById('inputDefense').value = 1;
+		}
+
+		var v = Math.ceil(defs*0.5);
+		var v2 = Math.ceil(defs*0.75);
+		var d = Math.ceil(dmgs-v);
+		var d2 = Math.ceil(dmgs-v2);
+		
+		document.getElementById("resultN").innerHTML = v;
+		document.getElementById("resultE").innerHTML = v2;
+		document.getElementById("resultND").innerHTML = d;
+		document.getElementById("resultED").innerHTML = d2;
+		
+	}
+}
